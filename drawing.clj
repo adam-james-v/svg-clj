c00baef3fa8c193031d81206a231c86b547a2529
Adam James <adam.vermeer2@gmail.com>
2021-06-13 15:58:03 -0700

    working binary with watcher. at least on macos

--- drawing.clj
+++ drawing.clj
@@ -6,7 +6,7 @@
             [svg-clj.path :as path]
             [svg-clj.transforms :as tf]
             [svg-clj.layout :as lo]))
- 
+
 (defn rand-rect
   []
   (-> (el/rect 10 #_(+ 5 (rand-int 20)) 10 #_(+ 5 (rand-int 20)))
@@ -22,7 +22,7 @@
 (def dwg2
   (svg
    (el/g
-    (for [step (range 1 7 1)]
+    (for [step (range 1 5 1)]
       (lo/distribute-on-curve
        (repeatedly (* 10 step) rand-rect)
        (lo/p-circle (* 50 step)))))))
